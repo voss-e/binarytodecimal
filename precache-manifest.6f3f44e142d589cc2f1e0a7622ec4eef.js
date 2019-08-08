24f6710d31c695026024f48b8b17d94c076fb277
voss-e <jovicevicdev@gmail.com>
2019-08-08 18:22:30 +0200

    Updates

---
+++ precache-manifest.6f3f44e142d589cc2f1e0a7622ec4eef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb3d533cbea603d5de23eca33cec9d31",
+    "url": "/binarytodecimal/index.html"
+  },
+  {
+    "revision": "d00ced78ef4449ec7646",
+    "url": "/binarytodecimal/static/css/main.b3ae0f0b.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/binarytodecimal/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "d00ced78ef4449ec7646",
+    "url": "/binarytodecimal/static/js/main.f4c302e1.chunk.js"
+  },
+  {
+    "revision": "73c983afe6af4ccf0733",
+    "url": "/binarytodecimal/static/js/runtime~main.91e0fef5.js"
+  }
+]);
